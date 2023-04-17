447f245dc9ec35d85f4f1b525742e494ffe2a5c4
Maxwell T <43257438+Max-Tee@users.noreply.github.com>
2023-04-17 16:22:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 import streamlit as st 
 
-st.write("This is outside the container")
+st.title("Zone Planner")
+st.write("Navigating local zoning laws to get community projects done can be challenging, but AI can help.")
+st.button("Get started")
